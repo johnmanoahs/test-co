32f34aa8b5712bb64a573d0eac41cf23d92d8346
johnmanoahs <john.manoah@gmail.com>
2016-06-19 02:10:25 +0530

    updates

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ coApp.service("coAppService", function($http, $q){
 
 .controller('shCtrl', function($scope){
 	this.selunselt = true;
-	console.log(this.selunselt)
+	//console.log(this.selunselt)
 	$scope.selunsel = function(){
 		if(this.selunselt){
 			$scope.selunselc = 'unselected';
@@ -40,7 +40,33 @@ coApp.service("coAppService", function($http, $q){
 			$scope.selunselc = 'selected';
 		}
 		this.selunselt = !this.selunselt;
-		console.log(this.selunselt)
+		//console.log(this.selunselt)
 	}
 	//$scope.$watch()
-});
+})
+
+coApp.directive("contentBoxes", function(){
+	var DomListenerFn = function(scope,element,attrs){
+		var zoomout1 = function(){
+			//console.log(this);
+			//alert('hi');
+			$(this).animate({
+				height: '+=50'
+			})
+		};
+		//console.log($('div.Pending').on('click', zoomout1));
+		console.log('sdf');
+		//$('#build_432462').on('click', zoomout1);
+		//$('#build_432462').trigger('click');
+		$('#one').on('click', zoomout1);
+		//$('#one').trigger('click')
+	};
+
+	return{
+		replace: false,
+		restrict: 'AEC',
+		controller: 'shCtrl',
+		link: DomListenerFn,
+		transclude: false
+	}
+})
---
+++ js/appjq.js
@@ -0,0 +1,29 @@
+var myApp = angular.module('myApp', []);
+
+myApp.directive('myWidget', function() {
+    var linkFn;
+    linkFn = function(scope, element, attrs) {
+        var animateDown, animateRight, pageOne, pageTwo;
+        pageOne = angular.element(element.children()[0]);
+        pageTwo = angular.element(element.children()[1]);
+
+        animateDown = function() {
+            $(this).animate({
+                top: '+=50'
+            });
+        };
+
+        animateRight = function() {
+            $(this).animate({
+                left: '+=50'
+            });
+        };
+
+        $(pageOne).on('click', animateDown);
+        $(pageTwo).on('click', animateRight);
+    };
+    return {
+        restrict: 'E',
+        link: linkFn
+    };
+});
