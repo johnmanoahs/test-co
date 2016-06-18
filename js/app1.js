e27076c1fc14fd57420c6e21cbc5e3dc9c3561cd
johnmanoahs <john.manoah@gmail.com>
2016-06-18 18:31:02 +0530

    updates

--- js/app1.js
+++ js/app1.js
@@ -29,6 +29,8 @@ coApp.service("coAppService", function($http, $q){
 	}
 })
 
+.constant('prevSelected', '')
+
 .controller('shCtrl', function($scope){
 	this.selunselt = true;
 	console.log(this.selunselt)
@@ -42,5 +44,36 @@ coApp.service("coAppService", function($http, $q){
 		this.selunselt = !this.selunselt;
 		console.log(this.selunselt)
 	}
-	//$scope.$watch()
+})
+
+.controller("showHideCtrl", ['prevSelected', function($scope, $watch, prevSelected){
+	this.sel = null;
+	this.num = 0 ;
+	this.prevval = 0;
+	console.log(this.prevSel);
+	//console.log(prevSelected);
+	this.expandthis = function(rowindex, rowid, rowelement){
+		/*console.log(prevSelected)
+		$scope.sel = rowid;
+		angular.element(rowevent.target).addClass('selected');
+		if(prevSelected != ''){
+			var prevSelDiv = angular.element( document.querySelector( '#'+prevSelected ) );
+			prevSelDiv.removeClass('selected');
+			prevSelected = rowid;
+		}
+		*/
+		this.num = rowindex;
+		this.ell = rowelement
+		console.log(this.prevSel);
+	}
+	this.removePrevSel = function(){
+		this.prevval = this.num;
+	}
+}])
+.directive("prevVal", function($animate){
+	return function(scope, element, attrs){
+		scope.$watch(attrs.prevval, function(newVal){
+			$animate.removeClass(element, "selected")
+		})
+	}
 });
